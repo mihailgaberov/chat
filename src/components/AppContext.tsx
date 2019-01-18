f803096e94ea9f2603f7618c822bf8ac0b009e89
mihailgaberov <mihail.gaberov@gmail.com>
2019-01-18 18:09:39 +0100

    Added typings for making json import possible. Added Context HOC.

---
+++ src/components/AppContext.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+
+export interface IAppContext {
+    lang: string;
+    nav: {
+        chatTabLabel: string,
+        settingsTabLabel: string
+    };
+}
+
+const context = React.createContext<IAppContext | null>(null);
+
+export const AppContextProvider = context.Provider;
+
+export const AppContextConsumer = context.Consumer;
+
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
+export function withAppContext<P extends { appContext?: IAppContext }, R = Omit<P, "appContext">>(
+    WrappedComponent: React.ComponentClass<P> | React.FunctionComponent<P>
+): React.FunctionComponent<R> {
+    return function BoundComponentHOC(props: R) {
+        return (
+            <AppContextConsumer>
+                {value => <WrappedComponent {...props as any} appContext={value} />}
+            </AppContextConsumer>
+        );
+    };
+}
