0ae4e20ec01ddf1a20733818c5c189729996ac8a
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-01-21 12:40:26 +0100

    Updated LanguageSelector component - styles, translations, func.

--- src/components/LanguageSelector/StyledLanguageSelector.tsx
+++ src/components/LanguageSelector/StyledLanguageSelector.tsx
@@ -4,6 +4,15 @@ const StyledStyledLanguageSelector = styled("div")`
  padding: 14px;
  text-align: left;
  border-bottom: 1px solid ${props => props.theme.quaternaryColor};
+ 
+ select {
+  width: 100%;
+  line-height: 28px;
+  margin-top: 7px;
+  font-size: 1.1em;
+  box-sizing: border-box;
+  padding: 7px;
+ }
 `;
 
 export default StyledStyledLanguageSelector;
