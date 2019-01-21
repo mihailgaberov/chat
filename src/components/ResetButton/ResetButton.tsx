4e5fdb8450aa36a4efcc6947cf6df8d761a14b89
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-01-21 12:51:03 +0100

    Updated ResetButton component - styles, translations, func.

--- src/components/ResetButton/ResetButton.tsx
+++ src/components/ResetButton/ResetButton.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
-
 import StyledResetButton from './StyledResetButton';
+import { clearAll } from '../../utilities/localStorageService';
+
+const resetSettings = () => clearAll();
 
 const ResetButton: React.FunctionComponent = () => (
-  <StyledResetButton><button>Reset button</button></StyledResetButton>
+  <StyledResetButton><button onClick={resetSettings}>Reset button</button></StyledResetButton>
 );
 
 export default ResetButton;
--- src/components/ResetButton/StyledResetButton.tsx
+++ src/components/ResetButton/StyledResetButton.tsx
@@ -1,7 +1,18 @@
 import styled from '../../theme';
 
 const StyledResetButton = styled("div")`
- border: 1px solid aqua;
+  width: 100%;
+  position: fixed;
+  bottom: 14px;
+  
+  button {
+    width: 100%;
+    line-height: 28px;
+    font-size: 1.1em;
+    padding: 7px;
+    color: ${(props) => props.theme.primaryColor};
+    background-color: ${(props) => props.theme.tertiaryColor};
+  }
 `;
 
 export default StyledResetButton;
