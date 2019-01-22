4fe4389aa4db59c9d602a11219159f5bfc171fab
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-01-22 17:24:36 +0100

    Styled ChatArea component.

--- src/components/ChatArea/StyledChatArea.tsx
+++ src/components/ChatArea/StyledChatArea.tsx
@@ -3,6 +3,11 @@ import styled from '../../theme';
 const StyledChatArea = styled("section")`
   color: ${(props) => props.theme.primaryLightColor};
   background-color: ${(props) => props.theme.secondaryDarkColor};
+  display: inline-block;
+  position: absolute;
+  bottom: 88px;
+  width: 100%;
+  left: 0
 `;
 
 export default StyledChatArea;
