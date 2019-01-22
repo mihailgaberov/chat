16ed29790244190aa51e63f55d111fa9190bd6d5
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-01-22 17:05:34 +0100

    Styling the messages.

--- src/components/ChatArea/ChatArea.tsx
+++ src/components/ChatArea/ChatArea.tsx
@@ -6,6 +6,8 @@ const ChatArea = () => (
   <StyledChatArea>
     <Message type={'sent'} value={'Hi, asl pls?'}/>
     <Message type={'received'} value={'Hi, Doroteya. 21/f/sf'}/>
+    <Message type={'sent'} value={'How are you?'}/>
+    <Message type={'received'} value={'I am fine. Thank you! How are about you?'}/>
   </StyledChatArea>
 );
 
