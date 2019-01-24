5a8067d87833598037c76e4972668ee4b769090b
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-01-24 16:28:28 +0100

    Added helper functions for getting the current time in 12 or 24h mode.

--- src/components/MessageSender/MessageSender.tsx
+++ src/components/MessageSender/MessageSender.tsx
@@ -6,6 +6,7 @@ import StyledMessageSender from './StyledMessageSender';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { readRecord } from '../../utilities/localStorageService';
+import { getTime12Hours, getTime24hours } from '../../utilities/common';
 
 interface IMessageSenderDispatchProps {
   sendMessage: (message: {from: string, content: string}) => void;
@@ -37,12 +38,16 @@ export class MessageSender extends React.Component {
   private handleClick = () => {
     const { username, chatMessage } = this.state;
     // @ts-ignore
-    this.props.sendMessage({ from: username, content: chatMessage });
+    this.props.sendMessage({ from: username, content: chatMessage, time: this.getTime() });
+  };
+
+  private getTime = (): string => {
+    return readRecord('clockMode') === '12' ? getTime12Hours() : getTime24hours();
   };
 }
 
 const mapDispatchToProps = (dispatch: Dispatch<any>): IMessageSenderDispatchProps => ({
-  sendMessage: (message: { from: string, content: string}) => dispatch(sendMessage(message)),
+  sendMessage: (message: { from: string, content: string, time: string}) => dispatch(sendMessage(message)),
 });
 
 export default connect(null, mapDispatchToProps)(MessageSender);
---
+++ src/utilities/common.ts
@@ -0,0 +1,30 @@
+export const getTime24hours = (): string => {
+  const now = new Date();
+  let mins: number | string = now.getMinutes();
+  let hours: number | string = now.getHours();
+
+  if(hours < 10) {
+    hours = `0${hours}`
+  }
+
+  if (mins < 10) {
+    mins = `0${mins}`
+  }
+
+  return `${hours}:${mins}`
+};
+
+export const getTime12Hours = (): string => {
+  const now = new Date();
+  let mins: number | string = now.getMinutes();
+  let hours: number | string = now.getHours();
+
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+
+  if (mins < 10) {
+    mins = `0${mins}`
+  }
+
+  return `${hours}:${mins}`
+};
