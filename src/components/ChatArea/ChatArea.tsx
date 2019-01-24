bf4a2263380e27a7e0becd86332ea8610e17aa15
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-01-24 16:37:37 +0100

    Fixed setting the proper theme when no settings are recorded to LS.

--- src/components/ChatArea/ChatArea.tsx
+++ src/components/ChatArea/ChatArea.tsx
@@ -14,19 +14,19 @@ interface IChatAreaStare {
 class ChatArea extends React.Component {
   public render() {
     // @ts-ignore
-    // const { messages } = this.props;
-    const { messages } = {messages: [{from: 'Mihail', content: 'Hoi'},
-        {from: 'Mihail', content: 'Do you want to bang tonight?'}]};
+    const { messages } = this.props;
+    /*const { messages } = {messages: [{from: 'Mihail', content: 'Hoi', time: '11:32'},
+        {from: 'Mihail', content: 'Do you want to bang tonight?', time: '11:34'}]};*/
     console.log('>>> messages: ', messages);
 
     return (
       <StyledChatArea>
-        {messages.map((element: { from: string, content: string}, idx: number) => {
+        {messages.map((element: { from: string, content: string, time: string}, idx: number) => {
           return (
             <React.Fragment key={idx}>
               <div id='nickname-container'>
                 <Nickname value={element.from}/>
-                <Timestamp value={'10:30'}/>
+                <Timestamp value={element.time}/>
               </div>
               <Message type={'received'} value={element.content}/>
             </React.Fragment>
--- src/utilities/TranslationsProvider.tsx
+++ src/utilities/TranslationsProvider.tsx
@@ -41,7 +41,7 @@ export const AppContextConsumer = context.Consumer;
 export default class TranslationProvider extends React.Component {
   public state = {
     translations: readRecord('lang') !== 'de' ? translationsEN : translationsDE,
-    colorTheme: readRecord('theme') !== 'light' ? darkTheme : lightTheme
+    colorTheme: readRecord('theme') !== 'dark' ? lightTheme : darkTheme
   };
 
   public render() {
