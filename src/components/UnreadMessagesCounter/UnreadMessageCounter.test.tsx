d2115e1c62fafaf3d1497117bc9d87a3502a2785
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-01-26 16:46:49 +0100

    Added more unit tests - UnreadMessageCounter component.

--- src/components/UnreadMessagesCounter/UnreadMessageCounter.test.tsx
+++ src/components/UnreadMessagesCounter/UnreadMessageCounter.test.tsx
@@ -2,13 +2,20 @@ import {shallow} from 'enzyme';
 import * as React from 'react';
 import UnreadMessageCounter from './UnreadMessageCounter';
 
-const setup = () => {
-  return shallow(<UnreadMessageCounter count={23}/>)
+const setup = (count = 1) => {
+  return shallow(<UnreadMessageCounter count={count}/>)
 };
 
 describe('UnreadMessageCounter component', () => {
   it('renders without crashing', () => {
     const wrapper = setup();
     expect(wrapper).not.toBe(null)
+  });
+
+  it('should render only when count > 0', () => {
+    let wrapper = setup(0);
+    expect(wrapper.text()).toBe('');
+    wrapper = setup(12);
+    expect(wrapper.text()).toBe('12');
   });
 });
