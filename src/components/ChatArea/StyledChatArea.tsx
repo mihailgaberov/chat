4af5c38bcc899ea1791a894cd2b0181345cf7ac5
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-01-26 13:34:32 +0100

    Optimizing styles for different resolutions.

--- src/components/ChatArea/StyledChatArea.tsx
+++ src/components/ChatArea/StyledChatArea.tsx
@@ -6,13 +6,30 @@ const StyledChatArea = styled("section")`
   display: inline-block;
   position: absolute;
   width: 100%;
-  height: 100%;
+  height: calc( 100vh - 60px);
   overflow-y: scroll;
   left: 0;
   
   #nickname-container {
     clear: both;
   }
+  
+  /* ----------- Non-Retina Screens ----------- */
+@media screen 
+  and (min-device-width: 1200px) 
+  and (max-device-width: 1600px) 
+  and (-webkit-min-device-pixel-ratio: 1) {
+   overflow-y: hidden;
+}
+
+/* ----------- Retina Screens ----------- */
+@media screen 
+  and (min-device-width: 1200px) 
+  and (max-device-width: 1600px) 
+  and (-webkit-min-device-pixel-ratio: 2)
+  and (min-resolution: 192dpi) {
+  overflow-y: hidden;
+}
 `;
 
 export default StyledChatArea;
