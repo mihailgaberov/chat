6e5a80c9f84d47dd1eb66714b14769c9b7c8d7a6
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-01-26 16:44:08 +0100

    Added more unit tests - Timestamp component.

--- src/components/Timestamp/Timestamp.test.tsx
+++ src/components/Timestamp/Timestamp.test.tsx
@@ -2,13 +2,20 @@ import { shallow } from 'enzyme';
 import * as React from 'react';
 import Timestamp from './Timestamp';
 
-const setup = () => {
-  return shallow(<Timestamp value={'10:03'} floatToRight={true}/>)
+const setup = (value = '') => {
+  return shallow(<Timestamp value={value} floatToRight={true}/>)
 };
 
 describe('Timestamp component', () => {
   it('renders without crashing', () => {
     const wrapper = setup();
     expect(wrapper).not.toBe(null)
+  });
+
+  it('should be able to render properly value passed as prop', () => {
+    let wrapper = setup();
+    expect(wrapper.text()).toBe('');
+    wrapper = setup('11:22');
+    expect(wrapper.text()).toBe('11:22');
   });
 });
