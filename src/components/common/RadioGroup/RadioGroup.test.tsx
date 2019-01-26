8cad4eab5bbdbf222b4d04c8f7411e7e2c013c78
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-01-26 15:04:16 +0100

    Added more unit tests - RadioGroup component.

--- src/components/common/RadioGroup/RadioGroup.test.tsx
+++ src/components/common/RadioGroup/RadioGroup.test.tsx
@@ -2,11 +2,11 @@ import {shallow} from 'enzyme';
 import * as React from 'react';
 import RadioGroup from './RadioGroup';
 
-const setup = () => {
-  return shallow(<RadioGroup isLeftChecked={true}
+const setup = (isLeftChecked = true) => {
+  return shallow(<RadioGroup isLeftChecked={isLeftChecked}
                                 radioGroupName={'test'}
-                                leftRadioLabel={'Light'}
-                                rightRadioLabel={'Dark'}
+                                leftRadioLabel={'Left'}
+                                rightRadioLabel={'Right'}
                                 leftRadioValue={'Light'}
                                 rightRadioValue={'Dark'}
                                 callback={jest.fn()}
@@ -17,5 +17,15 @@ describe('RadioGroup component', () => {
   it('renders without crashing', () => {
     const wrapper = setup();
     expect(wrapper).not.toBe(null)
+  });
+
+  it('should render with left option checked', () => {
+    const wrapper = setup();
+    expect(wrapper.find('#Left').props().checked).toBe(true);
+  });
+
+  it('should render with right option checked', () => {
+    const wrapper = setup(false);
+    expect(wrapper.find('#Right').props().checked).toBe(true);
   });
 });
