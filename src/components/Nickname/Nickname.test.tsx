66f9079d13f157daf50dd4766f26cf9351f41d62
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-01-26 15:23:21 +0100

    Added more unit tests - Nickname component.

--- src/components/Nickname/Nickname.test.tsx
+++ src/components/Nickname/Nickname.test.tsx
@@ -2,13 +2,20 @@ import {shallow} from 'enzyme';
 import * as React from 'react';
 import Nickname from './Nickname';
 
-const setup = () => {
-  return shallow(<Nickname value={'guest0001'}/>)
+const setup = (username = '') => {
+  return shallow(<Nickname value={username}/>)
 };
 
 describe('Nickname component', () => {
   it('renders without crashing', () => {
     const wrapper = setup();
     expect(wrapper).not.toBe(null)
+  });
+
+  it('should render user name if given', () => {
+    let wrapper = setup();
+    expect(wrapper.text()).toBe('');
+    wrapper = setup('guest0001');
+    expect(wrapper.text()).toBe('guest0001');
   });
 });
