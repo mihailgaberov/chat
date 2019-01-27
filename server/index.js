514c44613004962ff15a11e58f14ce3d7dd9a5bf
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-01-27 11:25:56 +0100

    Preparing express socket server for Heroku.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const http = require('http').Server(app);
+const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 app.use(express.static(path.join(__dirname, '../build')));
