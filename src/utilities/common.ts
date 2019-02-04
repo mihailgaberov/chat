f1f59b55c5b7942679096cc1499524896b77c8d7
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-02-04 10:01:55 +0100

    Added am/pm to the time when 12 hours mode is selected.

--- src/utilities/common.ts
+++ src/utilities/common.ts
@@ -3,7 +3,7 @@ export const getTime24hours = (): string => {
   let mins: number | string = now.getMinutes();
   let hours: number | string = now.getHours();
 
-  if(hours < 10) {
+  if (hours < 10) {
     hours = `0${hours}`
   }
 
@@ -16,17 +16,13 @@ export const getTime24hours = (): string => {
 
 export const getTime12Hours = (): string => {
   const now = new Date();
-  let mins: number | string = now.getMinutes();
+  let minutes: number | string = now.getMinutes();
   let hours: number | string = now.getHours();
-
+  const ampm = hours >= 12 ? 'pm' : 'am';
   hours = hours % 12;
-  hours = hours ? hours : 12;
-
-  if (mins < 10) {
-    mins = `0${mins}`
-  }
-
-  return `${hours}:${mins}`
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  return `${hours}:${minutes}${ampm}`;
 };
 
 export const scrollToBottom = (elementToBeScrolled: Element): void => {
