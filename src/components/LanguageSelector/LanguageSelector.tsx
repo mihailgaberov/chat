e33d8b57ce60daf3d551bd1bee900eb28685a861
mihailgaberov <mihail.gaberov@gmail.com>
2020-08-24 23:41:47 +0300

    :lipstick: Formatting.

--- src/components/LanguageSelector/LanguageSelector.tsx
+++ src/components/LanguageSelector/LanguageSelector.tsx
@@ -6,23 +6,23 @@ import { IAppContext } from '../../utilities/TranslationsProvider';
 class LanguageSelector extends React.Component<{ translations: IAppContext, changeLanguage: () => void }> {
 
 
-  public render() {
-    const { translations } = this.props;
-    return (
-      <StyledLanguageSelector>
-        <label htmlFor='language'>{translations.langLabel}</label>
-        <select id='language' onChange={this.handleLanguageChange} defaultValue={readRecord('lang') || 'en'}>
-          <option value='en'>{translations.languageEN}</option>
-          <option value='de'>{translations.languageDE}</option>
-        </select>
-      </StyledLanguageSelector>
-    );
-  }
+    public render() {
+        const {translations} = this.props;
+        return (
+            <StyledLanguageSelector>
+                <label htmlFor='language'>{translations.langLabel}</label>
+                <select id='language' onChange={this.handleLanguageChange} defaultValue={readRecord('lang') || 'en'}>
+                    <option value='en'>{translations.languageEN}</option>
+                    <option value='de'>{translations.languageDE}</option>
+                </select>
+            </StyledLanguageSelector>
+        );
+    }
 
-  private handleLanguageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    storeToLocalStorage('lang', e.currentTarget.value);
-    this.props.changeLanguage();
-  };
+    private handleLanguageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        this.props.changeLanguage();
+        storeToLocalStorage('lang', e.currentTarget.value);
+    };
 }
 
 export default LanguageSelector;
