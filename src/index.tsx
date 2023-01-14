3b5c47b21d79b25ac8e1742418b3711911c4ffd3
Mihail Gaberov <mihail.gaberov@olx.com>
2023-01-14 14:28:14 +0100

    :rocket: Deploy to Vercel

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from "./App";
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -10,5 +9,4 @@ const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(<Provider store={store}>
     <App />
-</Provider>,);
-registerServiceWorker();
+</Provider>);
