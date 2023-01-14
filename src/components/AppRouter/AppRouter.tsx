5689b610b97b059914a461a3efcba076ab97ad86
Mihail Gaberov <mihail.gaberov@olx.com>
2023-01-14 12:52:25 +0100

    :construction: Migrating to latest versions of react, react-dom, react-router-dom and styled-components

--- src/components/AppRouter/AppRouter.tsx
+++ src/components/AppRouter/AppRouter.tsx
@@ -1,23 +1,22 @@
 import * as React from 'react';
-import { HashRouter, Route, RouteComponentProps, Switch, withRouter } from 'react-router-dom';
+import {HashRouter, Route, Routes} from 'react-router-dom';
 import Navigation from '../Navigation/Navigation';
 import ChatPage from '../pages/Chat/ChatPage';
 import SettingsPage from '../pages/Settings/SettingsPage';
-import { ComponentType } from 'react';
 
 // Fix React Router active class setting for redux connected components
-const BlockedNavigation = withRouter(Navigation as ComponentType<RouteComponentProps>);
+// const BlockedNavigation = withRouter(Navigation as ComponentType);
 
 export const AppRouter: React.FunctionComponent = () => {
   return (
     <HashRouter>
       <React.Fragment>
-        <BlockedNavigation />
-        <Switch>
-          <Route exact={true} path='/' component={ChatPage} />
-          <Route path='/chat' component={ChatPage} />
-          <Route path='/settings' component={SettingsPage} />
-        </Switch>
+        <Navigation />
+        <Routes>
+          <Route path='/' element={<ChatPage />} />
+          <Route path='/chat' element={<ChatPage />} />
+          <Route path='/settings' element={<SettingsPage />} />
+        </Routes>
       </React.Fragment>
     </HashRouter>
   );
