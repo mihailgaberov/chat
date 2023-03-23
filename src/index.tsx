4cca3be0e708b31a3779046e9361b2bf70345f4a
Caswel <121208942+Gaita1083@users.noreply.github.com>
2023-03-23 11:07:34 +0200

    updated json & index

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
-import * as React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from "./App";
+import React from 'react';
+import { createRoot } from 'react-dom';
+import App from './App';
 import './index.css';
 import { Provider } from 'react-redux';
 import store from './store';
 
 const container = document.getElementById('root');
-const root = createRoot(container!);
-root.render(<Provider store={store}>
+
+if (!container) {
+  throw new Error('Cannot find root container');
+}
+
+const root = createRoot(container);
+
+root.render(
+  <Provider store={store}>
     <App />
-</Provider>);
+  </Provider>
+);
