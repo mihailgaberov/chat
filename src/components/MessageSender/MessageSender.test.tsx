807f3b1c66ef17810f9f9abe2a7bcacafc8640f7
mihailgaberov <mihail.gaberov@gmail.com>
2023-12-31 12:55:54 +0100

    :recycle: update tests to use vitest and react-testing-library

--- src/components/MessageSender/MessageSender.test.tsx
+++ src/components/MessageSender/MessageSender.test.tsx
@@ -1,40 +1,22 @@
-import { shallow } from 'enzyme';
 import * as React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+
 import { MessageSender } from './MessageSender';
 
-jest.mock('../../utilities/localStorageService');
-
 const setup = () => {
-  // @ts-ignore
-  return shallow(<MessageSender sendMessage={jest.fn()}/>)
+  return render(<MessageSender />)
 };
 
-describe('MessageSender component', () => {
+describe('MessageSender', () => {
   it('renders without crashing', () => {
     const wrapper = setup();
     expect(wrapper).not.toBe(null)
   });
 
   it('should call sendChatMessage on click Send button', () => {
-    const wrapper = setup();
-    const instance = wrapper.instance();
-    // @ts-ignore
-    const spy = jest.spyOn(instance, 'sendChatMessage');
-    wrapper.find('button').simulate('click');
-    expect(spy).toHaveBeenCalledTimes(1);
+    setup();
+    fireEvent.click(screen.getByRole('button'))
+    expect(screen.getByRole('textbox')).toHaveValue('');
   });
-
-
-  it('should clear send messages input on click Send button', () => {
-    const wrapper = setup();
-    const instance = wrapper.instance();
-    instance.setState({ chatMessage: 'test' }, () => {
-      expect(wrapper.find('input').props().value).toBe('test');
-    });
-
-    wrapper.find('button').simulate('click');
-    expect(wrapper.find('input').props().value).toBe('');
-  });
-
-
 });
