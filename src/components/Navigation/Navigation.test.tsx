93d3acb5171a7cf69cb3c7bf94345d71d5800148
mihailgaberov <mihail.gaberov@gmail.com>
2023-12-31 13:05:16 +0100

    :recycle: update tests to use vitest and react-testing-library

--- src/components/Navigation/Navigation.test.tsx
+++ src/components/Navigation/Navigation.test.tsx
@@ -1,29 +1,37 @@
-import { shallow } from 'enzyme';
 import * as React from 'react';
-import { Navigation } from './Navigation';
-import translationsMock from '../../translations/translations-mock';
-import { NavLink } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+import { HashRouter } from 'react-router-dom';
 
-jest.mock('../../utilities/localStorageService');
+import translationsMock from '../../translations/translations-mock';
+
+import { Navigation } from './Navigation';
 
 const setup = () => {
-  return shallow(<Navigation messages={[]} username={'guest0001'}
-                             appContext={translationsMock} connectToSockets={jest.fn}/>)
+  return render(
+    <HashRouter>
+      <Navigation messages={[]} username={'guest0001'} appContext={translationsMock} connectToSockets={() => {}}/> 
+      </HashRouter>
+  );
 };
 
-describe('Navigation component', () => {
+describe('Navigation', () => {
   it('renders without crashing', () => {
     const wrapper = setup();
     expect(wrapper).not.toBe(null)
   });
 
   it('should have the right number navigation elements', () => {
-    const wrapper = setup();
-    expect(wrapper.find('li').length).toEqual(2);
+    setup();
+    const listItems = screen.getAllByRole('listitem');
+    expect(listItems).toHaveLength(2);
   });
 
   it('should render navigation links', () => {
-    const wrapper = setup();
-    expect(wrapper.find(NavLink).length).toEqual(2);
-  })
+    setup();
+    const links = screen.getAllByRole('link');
+    expect(links).toHaveLength(2);
+    expect(screen.getByText(/Chat/i)).toBeVisible();
+    expect(screen.getByText(/Settings/i)).toBeVisible();
+  });
 });
