c0003acee26be25d5a31cbbf6ed9357755d03de0
mihailgaberov <mihail.gaberov@gmail.com>
2023-12-31 13:14:01 +0100

    :recycle: update tests to use vitest and react-testing-library

--- src/components/ResetButton/ResetButton.test.tsx
+++ src/components/ResetButton/ResetButton.test.tsx
@@ -1,5 +1,5 @@
-import { render } from '@testing-library/react';
 import * as React from 'react';
+import { render } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 
 import translationsMock from '../../translations/translations-mock';
--- src/components/SendingOptions/SendingOptions.test.tsx
+++ src/components/SendingOptions/SendingOptions.test.tsx
@@ -1,15 +1,16 @@
-import {shallow} from 'enzyme';
 import * as React from 'react';
+import { render } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+
+
 import SendingOptions from './SendingOptions';
 import translationsMock from '../../translations/translations-mock';
 
-jest.mock('../../utilities/localStorageService');
-
 const setup = () => {
-  return shallow(<SendingOptions translations={translationsMock}/>)
+  return render(<SendingOptions translations={translationsMock}/>)
 };
 
-describe('SendingOptions component', () => {
+describe('SendingOptions', () => {
   it('renders without crashing', () => {
     const wrapper = setup();
     expect(wrapper).not.toBe(null)
