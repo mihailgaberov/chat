5541aa37737bbe1c900c21dc25dd18d54a760b54
mihailgaberov <mihail.gaberov@gmail.com>
2023-12-31 13:08:48 +0100

    :recycle: update tests to use vitest and react-testing-library

--- src/components/Nickname/Nickname.test.tsx
+++ src/components/Nickname/Nickname.test.tsx
@@ -1,21 +1,21 @@
-import {shallow} from 'enzyme';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
 import * as React from 'react';
+
 import Nickname from './Nickname';
 
 const setup = (username = '') => {
-  return shallow(<Nickname value={username}/>)
+  return render(<Nickname value={username}/>)
 };
 
-describe('Nickname component', () => {
+describe('Nickname', () => {
   it('renders without crashing', () => {
     const wrapper = setup();
     expect(wrapper).not.toBe(null)
   });
 
   it('should render user name if given', () => {
-    let wrapper = setup();
-    expect(wrapper.text()).toBe('');
-    wrapper = setup('guest0001');
-    expect(wrapper.text()).toBe('guest0001');
+    setup('guest0001');
+    expect(screen.getByText('guest0001')).toBeVisible();
   });
 });
