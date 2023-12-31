291d5e648ec7e302d3fd77a44d14bddc9cb44cfc
mihailgaberov <mihail.gaberov@gmail.com>
2023-12-31 12:44:50 +0100

    :recycle: update tests to use vitest and react-testing-library

--- src/components/Message/Message.test.tsx
+++ src/components/Message/Message.test.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+
 import Message from './Message';
 import Nickname from '../Nickname';
 import Timestamp from '../Timestamp';
 
 const setup = (type = 'received') => {
-  return shallow(<Message message={{from: 'guest0001', content: 'hi', time: '11:03', type}}/>)
+  return render(<Message message={{from: 'guest0001', content: 'hi', time: '11:03', type}}/>)
 };
 
 describe('Message component', () => {
@@ -15,14 +17,16 @@ describe('Message component', () => {
   });
 
   it('should render a message with nickname and time when receiving', () => {
-    const wrapper = setup();
-    expect(wrapper.find(Nickname).length).toBe(1);
-    expect(wrapper.find(Timestamp).length).toBe(1);
+    setup();
+    
+    expect(screen.getByText(/guest0001/i)).toBeVisible();
+    expect(screen.getByText(/11:03/i)).toBeVisible();
+    expect(screen.getByText(/hi/i)).toBeVisible();
   });
 
   it('should render a message with only time when sending', () => {
-    const wrapper = setup('sent');
-    expect(wrapper.find(Nickname).length).toBe(0);
-    expect(wrapper.find(Timestamp).length).toBe(1);
+    setup('sent');
+    expect(screen.getByText(/11:03/i)).toBeVisible();
+    expect(screen.getByText(/hi/i)).toBeVisible();
   });
 });
