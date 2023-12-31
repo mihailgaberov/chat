5a3cb12e583fa91c61ffbb26e3a3bef49260732f
mihailgaberov <mihail.gaberov@gmail.com>
2023-12-31 13:16:32 +0100

    :recycle: update tests to use vitest and react-testing-library

--- src/components/Timestamp/Timestamp.test.tsx
+++ src/components/Timestamp/Timestamp.test.tsx
@@ -1,21 +1,21 @@
-import { shallow } from 'enzyme';
 import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+
 import Timestamp from './Timestamp';
 
 const setup = (value = '') => {
-  return shallow(<Timestamp value={value} floatToRight={true}/>)
+  return render(<Timestamp value={value} floatToRight={true}/>)
 };
 
-describe('Timestamp component', () => {
+describe('Timestamp', () => {
   it('renders without crashing', () => {
     const wrapper = setup();
     expect(wrapper).not.toBe(null)
   });
 
   it('should be able to render properly value passed as prop', () => {
-    let wrapper = setup();
-    expect(wrapper.text()).toBe('');
-    wrapper = setup('11:22');
-    expect(wrapper.text()).toBe('11:22');
+    setup('11:22');
+    expect(screen.getByText('11:22')).toBeVisible();
   });
 });
