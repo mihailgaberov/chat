749819b7eba75972c94a2812565f96f89bfc5c0b
mihailgaberov <mihail.gaberov@gmail.com>
2023-12-31 15:31:04 +0100

    :recycle: update tests to use vitest and react-testing-library

--- src/components/UserProfile/UserProfile.test.tsx
+++ src/components/UserProfile/UserProfile.test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 
 import { UserProfile } from './UserProfile';
