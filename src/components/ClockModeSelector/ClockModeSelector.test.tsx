fd780370d6627f952a0c2a997816a47e77f195e7
mihailgaberov <mihail.gaberov@gmail.com>
2023-12-31 12:20:22 +0100

    :recycle: update tests to use vitest and react-testing-library

--- src/components/ClockModeSelector/ClockModeSelector.test.tsx
+++ src/components/ClockModeSelector/ClockModeSelector.test.tsx
@@ -1,17 +1,15 @@
-import {shallow} from 'enzyme';
 import * as React from 'react';
 import ClockModeSelector from './ClockModeSelector';
 import translationsMock from '../../translations/translations-mock';
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
 
-jest.mock('../../utilities/localStorageService');
 
-const setup = () => {
-  return shallow(<ClockModeSelector translations={translationsMock} />)
-};
-
-describe('ClockModeSelector component', () => {
-  it('renders without crashing', () => {
-    const wrapper = setup();
-    expect(wrapper).not.toBe(null)
+describe('ClockModeSelector', () => {
+  it('renders clock mode selector options', () => {
+    render(<ClockModeSelector translations={translationsMock} />);
+    expect(screen.getByText(/Clock Display/i)).toBeVisible()
+    expect(screen.getByText(/12 Hours/i)).toBeVisible()
+    expect(screen.getByText(/24 Hours/i)).toBeVisible();
   });
 });
